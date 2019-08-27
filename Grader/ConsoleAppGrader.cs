2023ea8766a178bb125976deced218c0d34f1afd
Chris Evans <chrisevans9629@gmail.com>
2019-08-27 13:42:19 -0500

    created code analyzer

---
+++ Grader/ConsoleAppGrader.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+namespace Grader
+{
+
+    public class TypeInferenceRewriter : CSharpSyntaxRewriter
+    {
+        private readonly SemanticModel _model;
+
+        public TypeInferenceRewriter(SemanticModel model)
+        {
+            _model = model;
+        }
+
+        public override SyntaxNode VisitLocalDeclarationStatement(LocalDeclarationStatementSyntax node)
+        {
+            if (node.Declaration.Variables.Count > 1)
+            {
+                return node;
+            }
+            if (node.Declaration.Variables[0].Initializer == null)
+            {
+                return node;
+            }
+
+            VariableDeclaratorSyntax declarator = node.Declaration.Variables.First();
+            TypeSyntax variableTypeName = node.Declaration.Type;
+
+            ITypeSymbol variableType =
+                (ITypeSymbol)_model.GetSymbolInfo(variableTypeName)
+                    .Symbol;
+
+            TypeInfo initializerInfo =
+                _model.GetTypeInfo(declarator
+                    .Initializer
+                    .Value);
+
+            if (variableType == initializerInfo.Type)
+            {
+                TypeSyntax varTypeName =
+                    IdentifierName("var")
+                        .WithLeadingTrivia(
+                            variableTypeName.GetLeadingTrivia())
+                        .WithTrailingTrivia(
+                            variableTypeName.GetTrailingTrivia());
+
+                return node.ReplaceNode(variableTypeName, varTypeName);
+            }
+            else
+            {
+                return node;
+            }
+        }
+    }
+
+    public class GradeResult
+    {
+
+    }
+    public class ConsoleAppGrader
+    {
+        public ConsoleAppGrader()
+        {
+            
+        }
+
+
+        public GradeResult Grade(string program)
+        {
+
+           
+
+
+
+            NameSyntax name = IdentifierName("System");
+            name = QualifiedName(name, IdentifierName("Collections"));
+            name = QualifiedName(name, IdentifierName("Generic"));
+
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(
+                @"using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+ 
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(""Hello, World!"");
+        }
+    }
+}");
+
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+            var oldUsing = root.Usings[1];
+            var newUsing = oldUsing.WithName(name);
+
+            root = root.ReplaceNode(oldUsing, newUsing);
+
+            Compilation test = CreateTestCompilation(root.SyntaxTree);
+
+
+            foreach (SyntaxTree sourceTree in test.SyntaxTrees)
+            {
+                SemanticModel model = test.GetSemanticModel(sourceTree);
+
+                TypeInferenceRewriter rewriter = new TypeInferenceRewriter(model);
+                SyntaxNode newSource = rewriter.Visit(sourceTree.GetRoot());
+
+                if (newSource != sourceTree.GetRoot())
+                {
+                    File.WriteAllText(sourceTree.FilePath, newSource.ToFullString());
+                }
+            }
+            
+            return null;
+        }
+
+        private Compilation CreateTestCompilation(SyntaxTree tree)
+        {
+            MetadataReference mscorlib =
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+            MetadataReference codeAnalysis =
+                MetadataReference.CreateFromFile(typeof(SyntaxTree).Assembly.Location);
+            MetadataReference csharpCodeAnalysis =
+                MetadataReference.CreateFromFile(typeof(CSharpSyntaxTree).Assembly.Location);
+
+            MetadataReference[] references = { mscorlib, codeAnalysis, csharpCodeAnalysis };
+
+            return CSharpCompilation.Create("Test", new[] {tree}, references, new CSharpCompilationOptions(OutputKind.ConsoleApplication));
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using Grader;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Grade()
+        {
+            var grader = new ConsoleAppGrader();
+
+           var result =  grader.Grade("");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
