358e7284be92c858909fb7bc41c184be5a8be3bb
Chris Evans <chrisevans9629@gmail.com>
2019-09-08 17:54:24 -0500

    creating student home page

---
+++ Grader/ViewModels/StudentHomeViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using Grader;
+
+namespace AsyncToolWindowSample.ToolWindows
+{
+    public class StudentHomeViewModel : BindableBase
+    {
+        private readonly IGradeBookRepository _repository;
+        private ObservableCollection<StudentProjectViewModel> _toDoList;
+        private ObservableCollection<StudentProjectViewModel> _inProgressList;
+        private ObservableCollection<StudentProjectViewModel> _submittedList;
+
+        public StudentHomeViewModel(IGradeBookRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Initialize()
+        {
+            
+        }
+
+        public ObservableCollection<StudentProjectViewModel> ToDoList
+        {
+            get => _toDoList;
+            set => SetProperty(ref _toDoList,value);
+        }
+
+        public ObservableCollection<StudentProjectViewModel> InProgressList
+        {
+            get => _inProgressList;
+            set => SetProperty(ref _inProgressList,value);
+        }
+
+        public ObservableCollection<StudentProjectViewModel> SubmittedList
+        {
+            get => _submittedList;
+            set => SetProperty(ref _submittedList,value);
+        }
+    }
+}
---
+++ Grader/ViewModels/StudentProjectViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Grader;
+
+namespace AsyncToolWindowSample.ToolWindows
+{
+    public class StudentProjectViewModel : BindableBase
+    {
+        public StudentProjectDto Project { get; }
+        public DelegateCommand NavigateToProjectViewCommand { get; set; }
+        public StudentProjectViewModel(StudentProjectDto studentProjectDto)
+        {
+            Project = studentProjectDto;
+        }
+    }
+}
