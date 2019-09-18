cc2b2e6cf9aa3f49ee4d4cb04b44d7dcd7ec24cd
Chris Evans <Cevans@peerlessproducts.com>
2019-09-17 21:37:11 -0500

    added some standard views for CRUD operations

---
+++ Grader.Core/ViewModels/ClassesViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using Grader;
+
+namespace AsyncToolWindowSample.ToolWindows
+{
+    public class ClassesViewModel : BindableViewModel
+    {
+        private ObservableCollection<Class> _classes;
+        private Class _selectedClass;
+
+        public Class SelectedClass
+        {
+            get => _selectedClass;
+            set => SetProperty(ref _selectedClass,value);
+        }
+
+        public ObservableCollection<Class> Classes
+        {
+            get => _classes;
+            set => SetProperty(ref _classes,value);
+        }
+
+        public ClassesViewModel()
+        {
+            Classes = new ObservableCollection<Class>();
+        }
+    }
+}
