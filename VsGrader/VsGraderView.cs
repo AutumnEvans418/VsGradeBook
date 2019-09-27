a65a77dc60f195778e56f9253da91b1ab9175e80
Chris Evans <Cevans@peerlessproducts.com>
2019-09-26 21:08:10 -0500

    login page worked!

--- VsGrader/VsGraderView.cs
+++ VsGrader/VsGraderView.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using AsyncToolWindowSample.ToolWindows;
+using Grader;
 using Microsoft.VisualStudio.Shell;
+using Unity;
 
 namespace VsGrader
 {
@@ -28,7 +31,11 @@ namespace VsGrader
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
             // the object returned by the Content property.
-            this.Content = new VsGraderViewControl();
+            var boot = new Bootstrapper();
+            var con = boot.Initialize();
+            var mainView = con.Resolve<MainView>();
+            this.Content = mainView;
+            mainView.ToPage("LoginView");
         }
     }
 }
