2b5772ae69e1e06e2b4453737e891646cd8e3a0e
Chris Evans <Cevans@peerlessproducts.com>
2019-09-26 20:08:39 -0500

    created new extension

---
+++ VsGrader/VsGraderView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace VsGrader
+{
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("8523bd13-1011-4835-8e33-26be5fc682c2")]
+    public class VsGraderView : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VsGraderView"/> class.
+        /// </summary>
+        public VsGraderView() : base(null)
+        {
+            this.Caption = "VsGraderView";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new VsGraderViewControl();
+        }
+    }
+}
