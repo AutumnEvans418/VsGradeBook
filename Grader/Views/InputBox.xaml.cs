a373f36a53ffdd58e73dea555d76f024835556f0
Chris Evans <Cevans@peerlessproducts.com>
2019-09-29 22:46:38 -0500

    created inputbox

---
+++ Grader/Views/InputBox.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Grader.Views
+{
+    public interface IInputBoxService
+    {
+        Task<string> Show(string title = null, string msg = null);
+    }
+    public class InputBoxService : IInputBoxService
+    {
+        public async Task<string> Show(string title = null, string msg = null)
+        {
+            var win = new InputBox(msg);
+            win.Title = title;
+            win.ShowDialog();
+            return win.TextBox.Text;
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for InputBox.xaml
+    /// </summary>
+    public partial class InputBox : Window
+    {
+        public InputBox(string msg)
+        {
+            InitializeComponent();
+            this.Label.Content = msg;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
